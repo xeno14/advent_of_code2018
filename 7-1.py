87d0f4dc76ca9cd82d68618255709e446cf09b37
Ryo Murakami <integral14@gmail.com>
2019-02-14 23:34:24 +0900

    topological sort

--- 7-1.py
+++ 7-1.py
@@ -1,37 +1,51 @@
 import string
 import pprint
+import collections
+import heapq
 
-if __name__ == '__main__':
-    m = dict()
 
-    froms = []
-    tos = []
-    #with open("7.test") as f:
+def topological_sort(g: dict):
+    # count number of edges into each node
+    ins = {u: 0 for u in g}
+    for u in g:
+        for v in g[u]:
+            ins[v] += 1
+
+    res = []
+
+    # use heap as a priority queue to follow alphabetical order
+    q = [u for u, n in ins.items() if n == 0]
+    heapq.heapify(q)
+    while q:
+        u = heapq.heappop(q)
+        res.append(u)
+
+        # remove edges from u
+        for v in g[u]:
+            ins[v] -= 1
+            if ins[v] == 0:
+                heapq.heappush(q, v)
+    return res
+
+
+def main():
+    import string
+    g = {u: [] for u in string.ascii_uppercase}
+
+    nodes = set()
+    # with open("input/7.test") as f:
     with open("input/7.txt") as f:
         for line in f:
             sp = line.split()
-            froms.append(sp[1])
-            tos.append(sp[7])
-    keys = set(froms + tos)
-    m = {k:set() for k in keys}
-    for frm, to in zip(froms, tos):
-        m[to].add(frm)
+            u, v = sp[1], sp[7]
+            g[u].append(v)
+            nodes.add(u)
+            nodes.add(v)
+    g = {u: g[u] for u in g.keys() if u in nodes}
 
-    ans = ""
-    print(m)
-    while len(m) > 0:
-        ready = sorted([k for k, v in m.items() if len(v) == 0])
-        print(ready)
-
-        r = ready[0]
-        ans += r
-        del m[r]
-        for k in m:
-            if r in m[k]:
-                m[k].remove(r)
+    ans = "".join(topological_sort(g))
     print(ans)
 
 
-
-
-
+if __name__ == '__main__':
+    main()
